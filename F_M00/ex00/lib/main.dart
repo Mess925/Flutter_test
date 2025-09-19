1cff34d54ba247ed821341ef6fda2f6f20c25bd5
Mess925 <hanminthant222@gmail.com>
2025-09-19 14:46:58 +0800

    42 mobile M00 ex00

---
+++ F_M00/ex00/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
+class MyApp extends StatelessWidget{
+	const MyApp({super.key});
+
+	@override
+		Widget build(BuildContext context){
+			return MaterialApp(
+					home: Scaffold(
+						body: Center(
+							child: Column(
+							mainAxisAlignment: MainAxisAlignment.center,
+							children: [
+							Text(
+							"HELLO"),
+							ElevatedButton(onPressed: (){
+								print("HI");
+								}, child: const Text("CLICK"),),
+							],
+								),
+							),
+						),
+					);
+		}
+}
+
+void main()
+{
+	runApp(const MyApp());
+}
+
